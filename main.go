8b7d6a42834aa069cd9c66829df042d80c9e9025
dk <81505285@qq.com>
2021-03-29 23:42:45 +0800

    测试gin运行

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("hello world!")
 	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "测试OK",
+		})
+	})
+	r.Run(":8080")
 
 }
