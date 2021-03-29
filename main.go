1be1adb5d70ee98a3e3a336b674bba5fafa392bb
dk <81505285@qq.com>
2021-03-29 23:51:49 +0800

    用户注册：验证数据

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("hello world!")
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/api/auth/register", func(c *gin.Context) {
+		//  获取参数
+		name := c.PostForm("name")
+		password := c.PostForm("password")
+		telephone := c.PostForm("telephone")
+		// 数据验证
+		if len(telephone) != 0 { // 判断手机号码是否存在
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 422,
+				"msg":  "手机号必须为11位",
+			})
+		}
+
+		if len(password) < 6 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"code": 422,
+				"msg":  "",
+			})
+		}
+
+		//  创建用户
+		//  返回结果
+
 		c.JSON(http.StatusOK, gin.H{
-			"msg": "测试OK",
+			"msg": "注册成功",
 		})
 	})
 	r.Run(":8080")
