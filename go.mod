9491bfbbfd019fb5eeadb07c147db50457de00d6
dk <81505285@qq.com>
2021-03-30 20:35:47 +0800

    用户注册：写入到数据库

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module viper
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
+	gorm.io/driver/mysql v1.0.5
+	gorm.io/gorm v1.21.6
+)
--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
+type User struct {
+	gorm.Model
+	Name      string `gorm:"type:varchar(20);not null"`
+	PassWord  string `gorm:"size 255;not null"`
+	TelePhone string `gorm:"type:varchar(11);not null"`
+}
+
 func main() {
+	db := InitDB()
 	r := gin.Default()
 	r.POST("/api/auth/register", func(c *gin.Context) {
 		//  获取参数
@@ -18,14 +28,12 @@ func main() {
 		// 数据验证
 
 		if len(telephone) != 0 {
-			c.JSON(http.StatusUnprocessableEntity, gin.H{
-				"code": 422, "msg": "手机号必须为11位"})
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
 			return
 		}
 
 		if len(password) < 6 {
-			c.JSON(http.StatusUnprocessableEntity, gin.H{
-				"code": 422, "msg": "密码不能少于6位"})
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
 			return
 		}
 
@@ -34,9 +42,21 @@ func main() {
 			name = RandomString(10)
 		}
 		log.Println(name, password, telephone)
-		//  创建用户
-		//  返回结果
 
+		// 判断手机号是否存在，如果存在不允许用户注册
+		if IsTelePhoneExist(db, telephone) {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已存在"})
+		}
+
+		//  如果用户不存在创建用户
+		newUser := User{
+			Name:      name,
+			PassWord:  password,
+			TelePhone: telephone,
+		}
+		db.Create(&newUser)
+
+		//  返回结果
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "注册成功",
 		})
@@ -53,4 +73,25 @@ func RandomString(n int) string {
 		b[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(b)
+}
+
+func InitDB() *gorm.DB {
+	dsn := "root:123.com@tcp(127.0.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database,err:" + err.Error())
+	}
+	return db
+}
+
+func IsTelePhoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where(telephone).First(&User{})
+	if user.ID != 0 {
+		return true
+	}
+	db.Table("users")
+	// 创建数据表
+	db.AutoMigrate(&User{})
+	return false
 }
