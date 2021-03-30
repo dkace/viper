f22b17e8fdcb53981c972a4d01adb1657c54db79
dk <81505285@qq.com>
2021-03-30 23:13:53 +0800

    用户注册

--- controller/usercontroller.go
+++ controller/usercontroller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
@@ -19,12 +20,12 @@ func Register(c *gin.Context) {
 	// 数据验证
 
 	if len(telephone) != 11 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": http.StatusUnprocessableEntity, "msg": "手机号必须为11位"})
 		return
 	}
 
 	if len(password) < 6 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": http.StatusUnprocessableEntity, "msg": "密码不能少于6位"})
 		return
 	}
 
@@ -36,21 +37,64 @@ func Register(c *gin.Context) {
 
 	// 判断手机号是否存在，如果存在不允许用户注册
 	if IsTelePhoneExist(DB, telephone) {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已存在"})
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": http.StatusUnprocessableEntity, "msg": "用户已存在"})
 		return
 	}
 
 	//  如果用户不存在创建用户
+	hasePassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": http.StatusInternalServerError, "msg": "加密错误"})
+		return
+	}
 	newUser := model.User{
 		Name:      name,
-		PassWord:  password,
+		PassWord:  string(hasePassword),
 		TelePhone: telephone,
 	}
 	DB.Create(&newUser)
 
 	//  返回结果
 	c.JSON(http.StatusOK, gin.H{
-		"msg": "注册成功",
+		"code": 200,
+		"msg":  "注册成功",
+	})
+}
+
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	// 获取参数
+	password := c.PostForm("password")
+	telephone := c.PostForm("telephone")
+	// 数据验证
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		return
+	}
+
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+		return
+	}
+	// 判断手机号是否正确
+	var user model.User
+	DB.Where("telephone=?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+	// 判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PassWord), []byte(password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+	// 发放token
+	token := "11"
+	// 返回结果
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登陆成功",
 	})
 }
 
--- model/user.go
+++ model/user.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name      string `gorm:"type:varchar(20);not null;column:name"`
-	PassWord  string `gorm:"size 255;not null;column:password"`
 	TelePhone string `gorm:"type:varchar(11);not null;column:telephone"`
+	PassWord  string `gorm:"size 255;not null;column:password"`
 }
